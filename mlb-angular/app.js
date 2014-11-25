b8ab47b51179467d460f9274055bad9fbe71c730
Albert Lyu <albertworld@gmail.com>
2014-11-24 20:36:05 -0600

    mlb-angular: Added input form and notes to each player

--- mlb-angular/app.js
+++ mlb-angular/app.js
@@ -1,13 +1,26 @@
 var App = angular.module('App', []);
 
 // Controller for roster data
-App.controller('RosterCtrl', function($scope, $http) {
+App.controller('RosterCtrl', ['$scope', '$http', function($scope, $http) {
   $http.get('roster.json')
     .then(function(res) {
       $scope.players = res.data;
+      $scope.players.forEach(function(player) {
+        player.notes = [];
+        player.text = '';
+        player.submit = function() {
+          if (player.text) {
+            player.notes.push(this.text);
+            player.text = '';
+          }
+          console.log(player);
+          console.log(player.notes);
+        }
+      })
+      console.log($scope.players);
       $scope.Math = Math;
     });
-});
+}]);
 
 // Directive for default player image
 App.directive('errSrc', function() {
@@ -26,9 +39,9 @@ App.directive('errSrc', function() {
 App.filter('ageFilter', function() {
   function calculateAge(dateOfBirth) {
     var birthdate = new Date(dateOfBirth);
-    var ageDifMs = Date.now()-birthdate.getTime();
+    var ageDifMs = Date.now() - birthdate.getTime();
     var ageDate = new Date(ageDifMs);
-    return Math.abs(ageDate.getUTCFullYear()-1970);
+    return Math.abs(ageDate.getUTCFullYear() - 1970);
   }
   return function(birthdate) {
     return calculateAge(birthdate);
