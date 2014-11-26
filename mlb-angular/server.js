0eb7057ab3ed3cf0389d95e21cb9a3f951c95139
Albert Lyu <albertworld@gmail.com>
2014-11-26 13:38:03 -0600

    mlb-angular: Update server.js for Heroku

--- mlb-angular/server.js
+++ mlb-angular/server.js
@@ -1,6 +1,6 @@
 var connect = require('connect');
 var serveStatic = require('serve-static');
 
-connect().use(serveStatic(__dirname)).listen(8000);
+connect().use(serveStatic(__dirname)).listen(process.env.PORT || 8000);
 
 console.log('Server running at http://localhost:8000/');
